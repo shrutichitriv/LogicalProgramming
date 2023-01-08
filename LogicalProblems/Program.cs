f3168378b063d186d2ac82e5a0f86acc769443ad
shrutichitriv <shrutichitriv199620@gmail.com>
2023-01-08 16:29:03 +0530

    [Shruti] Solve Logical Programs

---
+++ LogicalProblems/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Logical_Programming
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Logical Programs");
+        }
+    }
+}
+
